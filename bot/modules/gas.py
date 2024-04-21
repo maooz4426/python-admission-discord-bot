c62c4fcd51b30ae6ac94f5fe951588b2f0d2c9a1
MAOZ <maooz4426@gmail.com>
2024-04-21 14:40:34 +0900

    uidと日付を記述できるように

--- bot/modules/gas.py
+++ bot/modules/gas.py
@@ -7,7 +7,7 @@ url = os.getenv("URL")
 
 # class gas_post:
 #     def __init__(self,name,hiragana):
-def gas_post(name,hiragana):
+def gas_post(interaction,name,hiragana):
 
         print("post")
         #提出時間取得
@@ -16,6 +16,7 @@ def gas_post(name,hiragana):
 
         headers = {"Content-Type": "application/json", }
         data={
+            "uid":str(interaction.user.id),#str型にして、科学的表記法を回避
             "currentTime":format_now,
             "name":name,
             "hiragana":hiragana
@@ -25,7 +26,7 @@ def gas_post(name,hiragana):
         try:
             response = requests.post(url, data=json.dumps(data), headers=headers)
             response.raise_for_status()  # HTTPエラーをチェック
-            print(json.dumps(data))
-            print(response.text)
+            print(json.dumps(data)) #jsonの中身チェック
+            print(response.text) #テキスト形式で取得
         except requests.RequestException as e:
             print(f"Request failed: {e}")
--- bot/modules/modal_template.py
+++ bot/modules/modal_template.py
@@ -3,15 +3,16 @@ from discord.ui import Modal,InputText
 from discord import InputTextStyle
 from .gas import gas_post
 
-
+#モーダルのテンプレート
 class SetModal(Modal):
     def __init__(self,title):
         super().__init__(title=title)
 
         self.title = title
 
+        # 名前入力
         self.name = InputText(label="名前",style=InputTextStyle.short)
-
+        # ふりがな入力
         self.hiragana = InputText(label="ふりがな",style=InputTextStyle.short)
 
         self.add_item(self.name)
@@ -22,6 +23,7 @@ class SetModal(Modal):
         name = self.name.value
         hiragana = self.hiragana.value
 
-        gas_post(name,hiragana)
+        #gasにpostリクエスト送信
+        gas_post(interaction,name,hiragana)
 
         await interaction.response.send_message(f"{self.title}を送信しました")
