b2378b23346b0b2f6ca3dd54870fe75cadcc85e4
MAOZ <118872701+maooz4426@users.noreply.github.com>
2024-06-25 09:03:47 +0900

    Update buttonHandle.py

--- bot/modules/buttonHandle.py
+++ bot/modules/buttonHandle.py
@@ -5,9 +5,8 @@ from .gas import GasHandle
 import config
 import asyncio
 import re
-# import os
 from .roleHandle import RoleHandle
-# from .modalHandle import SetForm
+
 
 
 def SetButtonStyle(form):
@@ -30,11 +29,8 @@ class SetButtonToView(Button):
         super().__init__(label = label, style=style)
         self.user = user
         self.title = label
-        # self.view = viewとすると目的のviewが開かない、セッターを準備しろとエラーが出る
-        # self.view2 = view
         self.style = style
         self.comment = user+comment
-        # self.form = form
 
     @property
     def view(self):
@@ -46,9 +42,6 @@ class SetButtonToView(Button):
 
     async def callback(self,interaction:discord.Interaction):
 
-        # await interaction.response.send_message(content="ボタンを準備しています")
-        
-        # temp_message.delete()
         #15分作業可能にするため
         await interaction.response.defer(ephemeral=True)
         #ボタン準備のための表示
@@ -70,11 +63,6 @@ class SetButtonToView(Button):
             from .modalHandle import SetForm
             self.form.setdata(get_data)
 
-        # elif self.form.title == "退会届":
-        #     uid = formalizeUid(self.user)
-
-        #     get_data = GasHandle.gas_get(uid)
-        #     print("getdata")
 
         #退会届は別ボタンを実装する
         if self.form.title == "退会届":
@@ -98,30 +86,11 @@ class SetButtonToView(Button):
             # Modal 用の View をここで作成する（モーダル遷移ボタンを表示）
             self.view = SetModalView(user=self.user, label=self.title + "(1/1)", modal=self.modal, style=self.style, form=self.form)
 
-            # self.view = SetButtonToModal(user=self.user,label=self.title,modal=self.modal,style=self.style,form=self.form)
-            
-            # message = await interaction.response.send_message(self.comment, view=self.view,ephemeral=True)
-            # await temp_message.delete()
-            # await interaction.delete_original_response()
-
             #遅らせた後にメッセージを送るのでfollowupを使う
             message = await interaction.followup.send(content=self.comment, view=self.view,ephemeral=True)
 
         #ボタン準備メッセージを削除
         await temp.delete()
-
-            #modalを作る前に使うために上に書くのでコメントアウト
-            # if self.form.title == "情報変更届":
-            #     uid = re.sub("[<@>]","",self.user)
-
-            #     get_data = GasHandle.gas_get(uid)
-
-            #     print("getdata")
-            #     # print(get_data)
-            #     from .modalHandle import SetForm
-            #     self.form.setdata(get_data)
-                # print(self.form.data.get("name"))
-                
 
         config.last_messageID = message
 
@@ -134,7 +103,6 @@ class SetButtonToModal(Button):
         self.user = user
         self.modal = modal
         self.form = form
-        # self.interaciton = interaction
 
     #ボタンのコールバック関数
     #モーダル表示する
@@ -142,7 +110,7 @@ class SetButtonToModal(Button):
 
         
         await interaction.response.send_modal(self.modal)
-        # await interaction.response.defer()
+
 
 #ボタンを押したらgasにpostリクエストを送る
 class SetFinishButton(Button):
@@ -161,27 +129,11 @@ class SetFinishButton(Button):
 
         #ロール付与
         await RoleHandle(interaction).giveRole(title = self.form.title)
-        # role = discord.utils.get(interaction.guild.roles, name="サークル会員")
-        # print(role)
-        # member = interaction.user
-        # await member.add_roles(role)
-        # uid = re.sub("[<@>]","",self.user)
-        # user_member = interaction.guild.get_member(user_id = uid)
-        # role = discord.utils.get(interaction.guild.roles, name=os.getenv("CIRCLE_MEMBER_ROLE_ID"))
-        # user_member.add_roles(role)
-
-
-        # last_message = config.last_messageID
-        # await last_message.delete_original_response()
-
-        # message = await interaction.response.send_message(self.user+f"{self.label}の送信が完了しました",ephemeral=True)
         
         #defer()してメッセージを送る時、interaction.edit_original_response または interaction.followup.sendしか使えない
         last_message = await interaction.edit_original_response(content=self.user+f"{self.label}の送信が完了しました", view = None)
         await asyncio.sleep(20)
         await last_message.delete()
-
-        # await last_message.delete_original_response()
 
 #退会用ボタン
 class SetDeleteButton(Button):
@@ -192,7 +144,6 @@ class SetDeleteButton(Button):
 
 
     async def callback(self, interaction: discord.Interaction):
-        # await interaction.edit_original_response(content = "送信中です",view = None)
         await interaction.response.defer()
         await interaction.edit_original_response(content = "送信中です",view = None)
         GasHandle.gas_post_delete(interaction,data = self.form.data,title = self.form.title)
